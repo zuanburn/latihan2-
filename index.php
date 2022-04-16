f80937dc7b6bdcd6fbb8e00c15d73fde5eaafcb9
Muhammad Syazwan Bin Suzaimi <syazwansuzaimi21@gmail.com>
2022-04-16 12:08:23 +0800

    edit

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 	echo "test";
+	echo "<table>
+			<tr>
+				<td>Nama Pengguna</td>
+				<td><input type='text' /></td>
+			</tr>
+			<tr>
+				<td>Kata Laluan</td>
+				<td><input type='password'/></td>
+			</tr>
+			<tr>
+				<td><input type='submit' /></td>
+				<td></td>
+			</tr>	
+		</table>";
 ?>
